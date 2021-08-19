c87d8db53b4d398bc32112f4ac6c3f640b565abe
cjimenez <xcatherine.jimenez@gmail.com>
2021-08-18 21:41:49 -0400

    accepts 2 nums and operator

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ exports.__esModule = true;
 var readline_sync_1 = require("readline-sync");
 function aQuestion() {
     var anInput = readline_sync_1.question("Enter a number: ");
-    console.log(anInput);
+    var operator = readline_sync_1.question("Enter an operator: ");
+    var secondInput = readline_sync_1.question("Enter another number: ");
+    var firstNum = isInt(anInput);
+    console.log(firstNum);
+}
+function isInt(string) {
+    var unknownType = parseInt(string);
+    var isNum = !isNaN(unknownType);
+    return isNum;
 }
 aQuestion();
--- index.ts
+++ index.ts
@@ -2,7 +2,17 @@ import { question } from "readline-sync";
 
 function aQuestion(): void {
   const anInput: string = question("Enter a number: ");
-  console.log(anInput);
+  const operator: string = question("Enter an operator: ");
+  const secondInput: string = question("Enter another number: ");
+
+  const firstNum = isInt(anInput);
+  console.log(firstNum);
+}
+
+function isInt(string: string): boolean {
+  const unknownType = parseInt(string);
+  const isNum: boolean = !isNaN(unknownType);
+  return isNum;
 }
 
 aQuestion();
